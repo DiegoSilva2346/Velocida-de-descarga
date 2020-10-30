fe8f2c14c7db2ddc14b2a172887589e805bfeb6f
DiegoSilva2346 <diegenriquesilva@gmail.com>
2020-10-29 23:33:56 -0300

    version 1

---
+++ signalwifi.py
@@ -0,0 +1,44 @@
+from speedtest import Speedtest         
+import time     
+       
+import csv    
+import pandas as pd
+st = Speedtest()         
+list_signal=list()            
+list_time=list()      
+ti=0      
+       
+
+        
+def genera_lista():      
+  tiempo_acumulado=0    
+  for i in range(0,600):         
+   if i==0:   
+    speed=st.download()   
+    print(str(speed)+ "  " + str(i))   
+    list_signal.append(speed)         
+   
+
+    list_time.append(0)    
+           
+   else:       
+    ti=time.time()     
+    speed=st.download()
+    list_signal.append(speed)    
+    print(str(speed)+ "  " + str(i))   
+
+    list_time.append(time.time()-ti+tiempo_acumulado)    
+    tiempo_acumulado=time.time()-ti +tiempo_acumulado      
+  return list_signal,list_time               
+
+def guarda_csv():       
+  tupla=genera_lista()       
+  lista_tiempos=tupla[1]    
+  signal=tupla[0]       	
+
+  
+  df = pd.DataFrame ({"tiempo":lista_tiempos,"signal":signal})       
+  df.to_csv (r'C:/escritorio/Python/BBDD1/export_dataframe_clear_600new2.csv', index = False, header=True)  
+
+if __name__ == '__main__':
+   	guarda_csv()
